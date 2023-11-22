5d835309298c68240b22272091bcfa78aa13254d
CesarZarco <141696762+CesarZarco@users.noreply.github.com>
2023-11-22 15:38:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='tilima_square',
+setup(name='lima_latin',
       version='0.1',
       description='A Python package for something',
-      url='http://github.com/lima_latin_sqr',
+      url='http://github.com/CesarZarco/lima_latin_sqr',
       author='Yo y los rancios',
       author_email='za440752@uaeh.edu.mx',
       license='MIT',
