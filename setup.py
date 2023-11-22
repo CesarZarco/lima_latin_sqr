50428c15eb03ca9c3035870f8abbee58bdb8bc68
CesarZarco <141696762+CesarZarco@users.noreply.github.com>
2023-11-22 15:21:33 -0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tilima_square',
+      version='0.1',
+      description='A Python package for something',
+      url='http://github.com/rvf0068/minimal',
+      author='Yo y los rancios',
+      author_email='za440752@uaeh.edu.mx',
+      license='MIT',
+      packages=['tilima_square'],
+      zip_safe=False)
