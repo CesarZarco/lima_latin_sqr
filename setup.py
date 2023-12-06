be10697f38fd31fd132eaf7d54f7bd663a96c380
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2023-12-06 16:22:59 -0600

    Update setup.py
    Modifique la descripción

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='tilima_square',
       version='0.1',
-      description='A Python package for something',
+      description='Un paquete de Python para verificar que no hay cuadrados latinos mutuamente ortogonales de dimensión 6',
       url='http://github.com/CesarZarco/lima_latin_sqr',
       author='Cesar Zarco, Carlos Haro',
       author_email='za440752@uaeh.edu.mx, ha419189@uaeh.edu.mx',
