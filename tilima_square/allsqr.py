499ba228676a35581bdf3e6a47b7d497c3d642c1
CesarZarco <141696762+CesarZarco@users.noreply.github.com>
2023-12-10 15:13:56 -0600

    Update allsqr.py
    La funcion all_squares_permutations() ahora si muestra todos los cuadrados latinos. Cambie el idioma y añadí documentación

--- tilima_square/allsqr.py
+++ tilima_square/allsqr.py
@@ -1,33 +1,25 @@
-from itertools import permutations
+from itertools import permutations, combinations
+from sympy import Matrix
 
-def all_square_permutations(n):
+def square_permutations(n):
     """
     Generate a list of matrices of order N, all of which are Latin squares, using permutations from itertools.
+    It does not show all possible latin squares of that order.
 
     Parameters:
     - N (int): The order of the matrix and the elements it will contain (1, 2, ..., n).
 
     Returns:
-    - all_square_permutations(N): List of Latin squares that come from permutations as rows.
+    - square_permutations(N): List of Latin squares that come from permutations as rows.
     
     Example
+    ```python
     cuadrados_latinos = all_square_permutations(3)
-    cuadrados_latinos
+    print(cuadrados_latinos)
     [[[1, 2, 3], [2, 3, 1], [3, 1, 2]],
-     [[1, 2, 3], [3, 1, 2], [2, 3, 1]],
-     [[1, 3, 2], [3, 2, 1], [2, 1, 3]],
-     [[1, 3, 2], [2, 1, 3], [3, 2, 1]],
-     [[2, 1, 3], [1, 3, 2], [3, 2, 1]],
-     [[2, 1, 3], [3, 2, 1], [1, 3, 2]],
-     [[2, 3, 1], [3, 1, 2], [1, 2, 3]],
-     [[2, 3, 1], [1, 2, 3], [3, 1, 2]],
-     [[3, 1, 2], [1, 2, 3], [2, 3, 1]],
-     [[3, 1, 2], [2, 3, 1], [1, 2, 3]],
-     [[3, 2, 1], [2, 1, 3], [1, 3, 2]],
-     [[3, 2, 1], [1, 3, 2], [2, 1, 3]]]
-    """
-
-   
+    [[1, 2, 3], [3, 1, 2], [2, 3, 1]],...
+    ```   
+    """   
     # Generate a list of numbers form 1 to N
     numbers = list(range(1, n + 1))
 
@@ -51,4 +43,71 @@ def all_square_permutations(n):
             latin_sqrs.append(latin_sqr)
 
     # Return the list
+    return latin_sqrs
+
+
+def is_latin_square_columns(matrix):
+    """
+    Verify if a matrix from sympy is a Latin square. This is an auxiliary function for all_square_permutations(n).
+
+    Parameters:
+    - matrix (sympy.Matrix): It must not have repetitions of elements in the same row.
+
+    Returns:
+    - is_latin_square_columns(matrix): True if it is a Latin square, False otherwise.
+    """
+    n = matrix.shape[0]
+
+    # Verifies if each number appears only once per column.
+    # It is not necessary to verify the rows; permutations do not have repetitions.
+
+    for i in range(0, n):
+        for j in range(0, n):
+            if matrix.transpose().tolist()[j].count(i+1) != 1:
+                return False
+    return True
+
+
+def all_square_permutations(n):
+    """
+    Generate a the list of all latin squares of order n.
+
+    Parameters:
+    - n (int): The order of the matrix and the elements it will contain (1, 2, ..., n).
+
+    Returns:
+    - all_square_permutations(n): List of all Latin squares of order n.
+    
+    Example
+    ```python
+    cuadrados_latinos = all_square_permutations(3)
+    print(cuadrados_latinos)
+    ```
+    [[Matrix([
+     [1, 2, 3],
+     [2, 3, 1],
+     [3, 1, 2]]),
+     Matrix([
+     [1, 2, 3],
+     [3, 1, 2],
+     [2, 3, 1]]),...
+
+    Warning:
+    This function may have high time and space complexity for 'n' values larger that 3.
+    """
+    
+    numbers = range(1,n+1)
+    latin_sqrs = []
+    all_permutations = []
+    
+    # Generate all permutations, including equivalents
+    for permutation in permutations(numbers):
+        for i in range(n):
+            all_permutations.append(list(permutation[i:] + permutation[:i]))
+
+    for combination in combinations(all_permutations, n):
+        # Crea un cuadrado latino al añadir la permutación a sí misma, pero en orden inverso
+        latin_sqr = Matrix(list(combination))
+        if is_latin_square_columns(latin_sqr) and latin_sqr not in latin_sqrs:
+            latin_sqrs.append(latin_sqr)
     return latin_sqrs
