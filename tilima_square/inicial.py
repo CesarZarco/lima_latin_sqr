ca901b0603b67fba8ed76d34b97198727f660a8c
CesarZarco <141696762+CesarZarco@users.noreply.github.com>
2023-12-12 09:31:14 -0600

    Update inicial.py
    Cambie la funcion square_0 por una funcion que genera una matriz de ceros en una en su forma estandar. standard_matrix(n)

--- tilima_square/inicial.py
+++ tilima_square/inicial.py
@@ -1,13 +1,39 @@
-from itertools import permutations, combinations
+import numpy as np
 
-def square_0(N):
+def standard_matrix(n):
     """
-    This function is a simple utility for creating zero-filled square matrices of a specified order N.
+    Generate a matrix of order n in its standard form.
+
+    The standard form of the matrix is a zeros matrix with the first row
+    and the first column containing natural numbers from 1 to n in increasing order.
 
     Parameters:
-    - N (int): The order of the square matrix required.
+    - n (int): The order of the matrix and the maximum value for the elements in the
+              first row and column (1, 2, ..., n).
 
     Returns:
-    - square_0(N): List of order N, which elements are all lists of order N with zeros.
+    - np.ndarray: Matrix in its standard form represented by a NumPy array.
+
+    Example:
+    >>> standard_matrix(3)
+    array([[1, 2, 3],
+           [1, 0, 0],
+           [2, 0, 0]])
+
+    Raises:
+    - ValueError: If n is not a positive integer.
     """
-    return [[0 for _ in range(N)] for _ in range(N)]
+
+    # Validate input
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer.")
+
+    # Create a zeros matrix
+    S = np.zeros((n, n), dtype=int)
+    
+    # Initialize the first row and column
+    S[0, :] = np.arange(1, n + 1)
+    S[:, 0] = np.arange(1, n + 1)
+
+    return S
+
