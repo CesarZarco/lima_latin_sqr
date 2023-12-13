228bc86e4ed8ed6040988e21fce11dbf8aa20b4c
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2023-12-12 22:12:11 -0600

    Update __init__.py
    Agregue la inicialización de las nuevas funciones

--- tilima_square/__init__.py
+++ tilima_square/__init__.py
@@ -1,4 +1,6 @@
 """Initialization for latin square"""
-from tilima_square.asp import *
+from tilima_square.als import *
 from tilima_square.inicial import *
 from tilima_square.validas import *
+from tilima_square.asp import *
+from tilima_square.molsthree import *
