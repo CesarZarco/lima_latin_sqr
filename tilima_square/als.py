d1c598c977532540c1d220e3f0ffc36f351659cd
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2024-01-11 17:49:31 -0600

    Update als.py
    Actualización de función: latin_square

--- tilima_square/als.py
+++ tilima_square/als.py
@@ -13,13 +13,31 @@ def total_latin_square(N):
     global SOLUTIONS
     square = [[0 for _ in range(N)] for _ in range(N)]
     
-    print(f'Total solutions found: {latin_square(square,1)}')
-    print(f'Solutions found: ')
-    for solution in SOLUTIONS:
-        print(solution)
-        
-    # Reset results
-    TOTAL_SOLUTIONS = 0
+    if not isinstance(N, int) or N <= 0:
+        raise ValueError("N must be a positive integer.")
+    
+    if N == 1:
+        print(f'Total solutions found: 1')
+        print(f'Solutions found: [1]')
+    else:
+        if type == 'TOTAL':
+            print(f'Total solutions found: {len(total_latin_square(square,1))}')
+            print(f'Solutions found: ')
+            for solution in SOLUTIONS:
+                print(solution)
+
+        if type == 'REDUCED':
+            print(f'Total solutions found: {len(reduced_latin_square(square,1))}')
+            print(f'Solutions found: ')
+            for solution in SOLUTIONS:
+                print(solution)
+
+        if type == 'STANDARD':
+            print(f'Total solutions found: {len(standard_latin_square(square,1))}')
+            print(f'Solutions found: ')
+            for solution in SOLUTIONS:
+                print(solution)
+    # Return to 0 de values
     SOLUTIONS = [] 
 
 def latin_square(square, color):
