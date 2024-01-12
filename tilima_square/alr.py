9ce78da43089198c196437ff0733ca46c4ee0d55
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2024-01-11 18:04:36 -0600

    Create alr.py
    Se creo función llamada reduced_latin_square, la cuál: itera a través de posibles soluciones obtenidas de la función reduced_completions

---
+++ tilima_square/alr.py
@@ -0,0 +1,23 @@
+def reduced_latin_square(square, color):
+    """
+    Main function to initiate the generation of a Latin square.
+
+    Args:
+    - square (list): The Latin square under construction.
+    - color (int): The current color being placed in the square.
+
+    This function uses backtracking to explore all possible completions of the Latin square.
+    """
+    global SOLUTIONS
+
+    N = len(square)
+
+    for possible_solution in reduced_completions(square, color):
+        if possible(possible_solution):
+            if is_solution(possible_solution):
+                SOLUTIONS.append(possible_solution)
+            else:
+                if completable(possible_solution):
+                    if color + 1 <= N:
+                        reduced_latin_square(possible_solution, color + 1)
+    return SOLUTIONS
