718a757201edc0aa52be2bdff3a5cb8c3119df41
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2024-01-14 17:40:22 -0600

    Update als.py
    Agregue una linea que especifica que regresa la función total_latin_square

--- tilima_square/square_success/als.py
+++ tilima_square/square_success/als.py
@@ -5,13 +5,16 @@ SOLUTIONS = []  # List to store the found solutions
 
 def total_latin_square(N):
     """
-    Main function to initiate the generation of a Latin square.
+    Function to initiate the generation of a Latin square.
 
     Args:
     - square (list): The Latin square under construction.
     - color (int): The current color being placed in the square.
 
     This function uses backtracking to explore all possible completions of the Latin square.
+
+    Returns:
+    - list: List of all Latin squares without conditions.
     """
     global SOLUTIONS
 
