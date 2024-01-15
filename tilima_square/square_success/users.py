1a189654f44ccf33f854c6fe9307bbacf0c75b67
CesarZarco <141696762+CesarZarco@users.noreply.github.com>
2024-01-14 19:09:46 -0600

    Update users.py
    para resolver el issue de interaccion con el usuario

--- tilima_square/square_success/users.py
+++ tilima_square/square_success/users.py
@@ -5,44 +5,46 @@ from tilima_square.square_success.alt import *
 from tilima_square.square_success.mol_aux import *
 
 SOLUTIONS = []
-def latin_square(square, color):
+def latin_square(N, type = 'TOTAL'):
     """
     Main function to print the Latin squares.
 
     Args:
     - N (int): Order of the Latin squares required.
+    - type (str): Type of Latin squares required.
+        - 'TOTAL' for all Latin squares,
+        - 'REDUCED' for Latin squares with the first row as (1,...,n),
+        - 'STANDARD' for Latin squares with the first row and column as (1,...,n).
 
-    This function only calls latin_square to show results
+    This function only show results
     """
     
     global SOLUTIONS
     square = [[0 for _ in range(N)] for _ in range(N)]
     
-    if not isinstance(N, int) or N <= 0:
-        raise ValueError("N must be a positive integer.")
+    if not isinstance(N, int) or N <= 1:
+        raise ValueError("N must be a positive integer greater or equal to 2.")
     
-    if N == 1:
-        print(f'Total solutions found: 1')
-        print(f'Solutions found: [1]')
+    if type == 'TOTAL':
+        print(f'Total solutions found: {len(total_latin_square(square,1))}')
+        print(f'Solutions found: ')
+        for solution in SOLUTIONS:
+            print(solution)
+
+    elif type == 'REDUCED':
+        print(f'Total solutions found: {len(reduced_latin_square(square,1))}')
+        print(f'Solutions found: ')
+        for solution in SOLUTIONS:
+            print(solution)
+
+    elif type == 'STANDARD':
+        print(f'Total solutions found: {len(standard_latin_square(square,1))}')
+        print(f'Solutions found: ')
+        for solution in SOLUTIONS:
+            print(solution)
     else:
-        if type == 'TOTAL':
-            print(f'Total solutions found: {len(total_latin_square(square,1))}')
-            print(f'Solutions found: ')
-            for solution in SOLUTIONS:
-                print(solution)
-
-        if type == 'REDUCED':
-            print(f'Total solutions found: {len(reduced_latin_square(square,1))}')
-            print(f'Solutions found: ')
-            for solution in SOLUTIONS:
-                print(solution)
-
-        if type == 'STANDARD':
-            print(f'Total solutions found: {len(standard_latin_square(square,1))}')
-            print(f'Solutions found: ')
-            for solution in SOLUTIONS:
-                print(solution)
-    # Return to 0 de values
+        print("Invalid latin_square_type. Please choose 'TOTAL', 'REDUCED', or 'STANDARD'.")
+    # Reset variable
     SOLUTIONS = []
 
 def mols(N):
