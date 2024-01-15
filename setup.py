f557086db29745a87587f10b67ba3f46424b3edc
jcar2905 <141780211+jcar2905@users.noreply.github.com>
2024-01-14 18:48:39 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='tilima_square',
       author='Cesar Zarco, Carlos Haro',
       author_email='za440752@uaeh.edu.mx, ha419189@uaeh.edu.mx',
       license='MIT',
-      packages=find_packages(),
+      packages=['tilima_square', 'tilima_square.square_success'],
       zip_safe=False)
